cf1d764268eb36335c6daad2285d53ee734c9334
Medy <mannomedy@gmail.com>
2025-07-24 10:52:24 +0300

    add changes

--- phase2-project/src/App.jsx
+++ phase2-project/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-           <Route path="/book/:id" element={<BookPage />} />
      
-      </Routes>
     </Router>
   );
 }
