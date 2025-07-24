45b29ceefb84ea922fbe998f8a3dea92cea1304b
Medy <mannomedy@gmail.com>
2025-07-24 10:44:45 +0300

    add changes

--- phase2-project/src/App.jsx
+++ phase2-project/src/App.jsx
@@ -1,11 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard.jsx";
 
 
 function App() {
-  
-
   return (
-    
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+     
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
