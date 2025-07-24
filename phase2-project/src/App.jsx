aa3f8d95d53dfd5019da11b416de14034a0c072d
Medy <mannomedy@gmail.com>
2025-07-24 11:13:12 +0300

    add changes

--- phase2-project/src/App.jsx
+++ phase2-project/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
      
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
      
       </Routes>
     </Router>
